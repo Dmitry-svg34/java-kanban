ef76dd4e632c51b34c6189bb4e48fc21ec774ff0
Dmitry <dima4433862@yandex.ru>
2025-06-27 10:53:05 +0300

    Выполнение задания

---
+++ src/tasks/Epic.java
@@ -0,0 +1,18 @@
+package tasks;
+
+import java.util.ArrayList;
+
+public class Epic extends Task {
+
+    private final ArrayList<Integer> subTasksId;
+
+    public ArrayList<Integer> getSubTasks() {
+        return subTasksId;
+    }
+
+    public Epic(String name, String description) {
+        super(name, description, TaskStatus.NEW);
+        this.subTasksId = new ArrayList<>();
+    }
+
+}
